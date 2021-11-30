de485f1f8962339e7c883e9b2c2aa850a86082bc
Tal Mogendorff <71597455+TalMogendorff@users.noreply.github.com>
2021-11-30 20:41:39 +0200

    Project Uploaded

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"image/color"
+	"log"
+	"sync"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	screenWidth, screenHeight = 640, 360
+	boidCount = 1500
+	viewRadius = 13
+	adjRate = 0.040
+)
+
+var (
+	green = color.RGBA{R:10,G:255,B:50,A:255}
+	boids[boidCount] *Boid
+	boidMap [screenWidth + 1][screenHeight + 1] int
+	rWlock = sync.RWMutex{}
+)
+
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for _, boid := range boids {
+		screen.Set(int(boid.position.x+1), int(boid.position.y), green)
+		screen.Set(int(boid.position.x-1), int(boid.position.y), green)
+		screen.Set(int(boid.position.x), int(boid.position.y-1), green)
+		screen.Set(int(boid.position.x), int(boid.position.y+1), green)
+	}
+}
+
+func (g *Game) Layout(_, _ int) (w, h int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+
+	for i,row :=range boidMap{
+		for j:= range row{
+			boidMap[i][j] = -1
+		}
+	}
+
+	for i := 0 ; i<boidCount; i++{
+		createBoid(i)
+	}
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Tal in a box")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
